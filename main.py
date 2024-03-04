406a5f4451c7c57f13f604d3d1dd83c72889c1eb
RussellMorryson <118932212+RussellMorryson@users.noreply.github.com>
2024-03-04 18:32:48 +0000

    Cpmplete task

--- main.py
+++ main.py
@@ -1,44 +1,73 @@
+import uvicorn
 import databases
 import sqlalchemy
+
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
-import uvicorn
+
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
 app = FastAPI()
 
+#################################################################################################
+# DB
 DATABASE_URL = "sqlite:///mydatabase.db"
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
+
 
 users = sqlalchemy.Table(
     "users",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-                      sqlalchemy.Column("name", sqlalchemy.String(32)),
-                      sqlalchemy.Column("email", sqlalchemy.String(128)),
+    sqlalchemy.Column("name", sqlalchemy.String(32)),
+    sqlalchemy.Column("surname", sqlalchemy.String(64)),
+    sqlalchemy.Column("email", sqlalchemy.String(128)),
+    sqlalchemy.Column("password", sqlalchemy.String(512)),
+    sqlalchemy.Column("date", sqlalchemy.DateTime, default=datetime.utcnow),
+    sqlalchemy.Column("status", sqlalchemy.String(64)))
 
-                      )
+products = sqlalchemy.Table(
+        "products",
+        metadata,
+        sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+        sqlalchemy.Column("title", sqlalchemy.String(64)),
+        sqlalchemy.Column("description", sqlalchemy.String(64)),
+        sqlalchemy.Column("price", sqlalchemy.Float),
+        sqlalchemy.Column("quantity", sqlalchemy.Integer))
+
+orders = sqlalchemy.Table(
+        "orders",
+        metadata,
+        sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+        sqlalchemy.Column("user_id", sqlalchemy.Integer, foreign_key = "users.id"),
+        sqlalchemy.Column("product_id", sqlalchemy.Integer, foreign_key = "products.id"),
+        sqlalchemy.Column("date", sqlalchemy.DateTime, default=datetime.utcnow),
+        sqlalchemy.Column("status", sqlalchemy.String(64), default="In processing"))
+
 
 engine = sqlalchemy.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 metadata.create_all(engine)
 
+
+#################################################################################################
+# MODELS
 class UserIn(BaseModel):
     name: str = Field(max_length=32, nullable=False)
     surname:str = Field(max_length=64, nullable=False)
     email: str = Field(max_length=128, nullable=False, unique=True)
-    password: str = Field(max_length=128, nullable=False)
+    password: str = Field(nullable=False)
 
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
+    # def set_password(self, password):
+    #     self.password = generate_password_hash(password)
 
 class User(BaseModel):
     id: int = Field(primary_key=True)
     name: str = Field(max_length=32, nullable=False)
     surname:str = Field(max_length=64, nullable=False)
     email: str = Field(max_length=128, nullable=False, unique=True)
-    password: str = Field(max_length=128, nullable=False)
+    password: str = Field(nullable=False)
     date: datetime = Field(default=datetime.utcnow)
     status: str = Field(default="Active")
     
@@ -68,20 +97,46 @@ class Order(BaseModel):
     id: int = Field(primary_key=True)
     user_id: int = Field(foreign_key="user.id")
     product_id: int = Field(foreign_key = "product.id")
+    date: datetime = Field(default=datetime.utcnow)
     status: str = Field(max_length=64, default="In processing")
 
-
+#################################################################################################
+# CRUD COMMANDS
 
 @app.get("/fake_users/{count}")
-async def create_note(count: int):
+async def create_f_users(count: int):
     for i in range(count):
-        query = users.insert().values(name=f'user{i}', email=f'mail{i}@mail.ru')
+        query = users.insert().values(name=f'user{i}', 
+                                      surname=f'user{i}',
+                                      email=f'mail{i}@mail.ru',
+                                      password = generate_password_hash('pass' + str(i)),
+                                      #date = datetime.utcnow,
+                                      status = "Active")
+
         await database.execute(query)
     return {'message': f'{count} fake users create'}
-    
 
 
+@app.get("/fake_products/{count}")
+async def create_f_products(count: int):
+    for i in range(count):
+        query = products.insert().values(title=f'product{i}', 
+                                         description=f'desc{i}',
+                                         price=100.00+i,
+                                         quantity=i)
+        await database.execute(query)
+    return {'message': f'{count} fake products create'}
 
+
+@app.get("/fake_orders/{count}")
+async def create_f_orders(count: int):
+    for i in range(count):
+        query = orders.insert().values(user_id=i,
+                                       product_id = i,
+                                       #date = datetime.utcnow,
+                                       status = "In processing")
+        await database.execute(query)
+    return {'message': f'{count} fake orders create'}
 
 
 # Точка входа
